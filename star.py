9355b525fd83f46117344da17d5eb82c7a327092
woneychl <woneychl@gmail.com>
2025-09-09 11:24:53 +0900

    수정했습니다

--- star.py
+++ star.py
@@ -3,12 +3,10 @@ class Pyramid:
         self.height = height
 
     def print_star_pyramid(self):
-        for i in range(1, self.height + 1):
-            print(" " * (self.height - i) + "*" * (2 * i - 1))
+        for i in range(self.height, 1-1, -1):
+            print(" " * (self.height - i)+ "*" * (2 * i - 1))
 
 
 height = int(input("피라미드 높이를 입력하세요: "))
 p = Pyramid(height)
 p.print_star_pyramid()
-
-#commit 
